7a705d45a23ba43f19fdca1541916635fc7c4bb3
nairsumy <nair.sumy@gmail.com>
2021-04-28 15:31:38 -0700

    Initial Commit

---
+++ webapp/controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+	"sap/m/Text"
+], function (Text) {
+	"use strict";
+
+	new Text(
+		{text: "Welcome to my controls SAP UI5 page"}
+	).placeAt("content");
+
+//	alert("UI5 is ready");  <--- this was a simple JS script
+}); 
+
